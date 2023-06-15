24cb0caa6c9153de0672c21998ab872f6527d775
Maryam HOUTAK <maryam.htk@outlook.fr>
2023-06-15 19:21:41 +0200

    Fichier vehicule.php modifié

--- src/view/vehicule.php
+++ src/view/vehicule.php
@@ -158,6 +158,35 @@
       <!--  Header End -->
       <div class="container-fluid">
         <div class="container-fluid">
+        <a href="index.php?action=create" class="btn btn-primary">Ajouter vehicule</a>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Type</th>
+            <th>Nombre de place</th>
+            <th>Date d'aller</th>
+            <th>Actions</th>
+        </tr>
+
+        </thead>
+        <tbody>
+        <?php /** @var \app\models\Vehicule[] $data */
+        foreach ($data as $vehicule): ?>
+            <tr>
+                <td><?= $vehicule->getId_vehicule_festival() ?></td>
+                <td><?= $vehicule->getType() ?></td>
+                <td><?= $vehicule->getPlace() ?> $</td>
+                <td><?= $vehicule->getDatealler() ?></td>
+               
+                <td>
+                    <a href="index.php?action=edit&id=<?php echo $vehicule->getId_vehicule_festival() ?>" class="btn btn-success btn-sm">Modifier</a>
+                    <a onclick="return confirm('Voulez vous vraiment supprimer la vehicule <?= $vehicule->getType() ?>');" href="index.php?action=destroy&id=<?php echo $vehicule->getId_vehicule_festival() ?>" class="btn btn-danger btn-sm">Supprimer</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
          
     </div>
   </div>
