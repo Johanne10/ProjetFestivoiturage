34fb719c07a482ed0eb57b6f20cbf3914f4b9922
Maryam HOUTAK <maryam.htk@outlook.fr>
2023-06-15 23:03:19 +0200

    Fichier utilisateur.php modifié

--- src/view/utilisateur.php
+++ src/view/utilisateur.php
@@ -158,7 +158,36 @@
       <!--  Header End -->
       <div class="container-fluid">
         <div class="container-fluid">
-         
+        <a href="index.php?action=create" class="btn btn-primary">Ajouter vehicule</a>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Login</th>
+            <th>Mot de passe</th>
+            <th>Role</th>
+            <th>Actions</th>
+        </tr>
+
+        </thead>
+        <tbody>
+        <?php /** @var \app\models\Utilisateur[] $data */
+        foreach ($data as $utilisateur): ?>
+            <tr>
+                <td><?= $utilisateur->getId_utilis() ?></td>
+                <td><?= $utilisateur->getLogin() ?></td>
+                <td><?= $utilisateur->getMdp() ?></td>
+                <td><?= $utilisateur->getRole() ?> $</td>
+
+
+                <td>
+                    <a href="index.php?action=edit&id=<?php echo $utilisateur->getId_vehicule_festival() ?>" class="btn btn-success btn-sm">Modifier</a>
+                    <a onclick="return confirm('Voulez vous vraiment supprimer l'utilisateur <?= $utilisateur->getLogin() ?>');" href="index.php?action=destroy&id=<?php echo $utilisateur->getId_utilis() ?>" class="btn btn-danger btn-sm">Supprimer</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
     </div>
   </div>
   <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
